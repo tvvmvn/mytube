307da4e3623794903d7cfab55c16d6f9f4612720
tvvmvn <tvvmvn@gmail.com>
2023-10-26 11:59:38 +0900

    10/26

--- src/App.js
+++ src/App.js
@@ -51,8 +51,8 @@ export default function App() {
 
   return (
     <>
-      <header className="fixed w-screen top-0 bg-black">
-        <div className="flex justify-between h-12 px-4">
+      <header className="fixed w-full top-0 box-content bg-black">
+        <div className="flex justify-between h-12">
           <div className="flex items-center">
             <button
               className="px-4 h-full"
@@ -74,7 +74,7 @@ export default function App() {
               >
                 <path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/>
               </svg>
-              <h1 className="text-white ml-2">
+              <h1 className="ml-1 text-white font-semibold">
                 MyTube
               </h1>
             </div>
